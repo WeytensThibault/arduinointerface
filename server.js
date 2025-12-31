3daa10f03a1e58b89fda6df580fa431d358efb6c
Thibault Weytens <thibault.weytensm@student.howest.be>
2025-12-31 21:24:41 +0100

    commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { SerialPort } = require("serialport");
+
+const app = express();
+
+// ⚠️ pas dit aan naar jouw poort
+const port = new SerialPort({
+  path: "/dev/cu.usbserial-110", // Mac
+  baudRate: 9600,
+});
+
+app.get("/move/:angle", (req, res) => {
+  const angle = req.params.angle;
+  port.write(angle + "\n");
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Server draait op http://localhost:3000");
+});
